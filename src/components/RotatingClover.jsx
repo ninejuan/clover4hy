b803a4852bb36438fd19c52a2bf2af5c8a7b6a03
Juan Lee <ninejuan@protonmail.com>
2024-11-28 00:51:08 +0900

    add: separate leaves, stem

--- src/components/RotatingClover.jsx
+++ src/components/RotatingClover.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 
-const rotate = keyframes`
+// 잎 회전 애니메이션
+const rotateLeaves = keyframes`
   0% {
     transform: rotate(0deg);
   }
@@ -10,75 +11,87 @@ const rotate = keyframes`
   }
 `;
 
+// 전체 컨테이너
 const Container = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-  margin: 0;
   background-color: #f4f4f4;
+  margin: 0;
 `;
 
-const Clover = styled.div`
+// 클로버 Wrapper
+const CloverWrapper = styled.div`
   position: relative;
-  width: 150px;
-  height: 150px;
-  animation: ${rotate} 4s linear infinite;
+  width: 200px;
+  height: 200px;
 `;
 
+// 잎 회전 영역
+const Leaves = styled.div`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  animation: ${rotateLeaves} 4s linear infinite;
+`;
+
+// 네잎클로버 잎
 const Leaf = styled.div`
   position: absolute;
-  width: 80px;
-  height: 120px;
+  width: 100px;
+  height: 150px;
   background-color: #7bc96f;
-  border-radius: 50%;
+  border-radius: 50% 50% 0 0;
   transform-origin: bottom center;
 
   &:nth-child(1) {
     top: 0;
-    left: 35px;
-    transform: rotate(0deg);
+    left: 50%;
+    transform: translateX(-50%) rotate(45deg);
   }
-
   &:nth-child(2) {
-    top: 35px;
+    top: 50%;
     left: 0;
-    transform: rotate(90deg);
+    transform: translateY(-50%) rotate(135deg);
   }
-
   &:nth-child(3) {
-    top: 35px;
-    right: 0;
-    transform: rotate(-90deg);
-  }
-
-  &:nth-child(4) {
     bottom: 0;
-    left: 35px;
-    transform: rotate(180deg);
+    left: 50%;
+    transform: translateX(-50%) rotate(-135deg);
+  }
+  &:nth-child(4) {
+    top: 50%;
+    right: 0;
+    transform: translateY(-50%) rotate(-45deg);
   }
 `;
 
+// 줄기 (잎 뒤로 이동)
 const Stem = styled.div`
   position: absolute;
   bottom: -50px;
   left: 50%;
-  width: 10px;
-  height: 100px;
+  width: 15px;
+  height: 400px;
   background-color: #7bc96f;
+  border-radius: 5px;
   transform: translateX(-50%);
+  z-index: 1; /* 잎 뒤로 이동 */
 `;
 
 const RotatingClover = () => {
   return (
     <Container>
-      <Clover>
-        <Leaf />
-        <Leaf />
-        <Leaf />
-        <Leaf />
+      <CloverWrapper>
         <Stem />
-      </Clover>
+        <Leaves>
+          <Leaf />
+          <Leaf />
+          <Leaf />
+          <Leaf />
+        </Leaves>
+      </CloverWrapper>
     </Container>
   );
 };
--- src/pages/dev.jsx
+++ src/pages/dev.jsx
@@ -1,12 +1,64 @@
 import React from "react";
 import styled from "styled-components";
-import RotatingClover from "../components/RotatingClover";
+
+const LuckyCloverContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: "Pretendard", sans-serif;
+`;
+
+const LuckyCloverText = styled.div`
+  font-family: "Noto Sans KR", "Pretendard Variable", Pretendard, sans-serif;
+  font-weight: 600;
+  font-size: 20px;
+  text-align: center;
+  margin-bottom: 20px;
+`;
+
+const LuckyCloverWrapper = styled.div`
+  position: relative;
+  width: 350px;
+  height: 600px;
+`;
+
+const LuckyCloverLeaves = styled.div`
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 300px;
+  height: 300px;
+  background-image: url("/leaves.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
+
+const LuckyCloverStem = styled.div`
+  position: absolute;
+  top: 55%;
+  left: 50%;
+  transform: translate(-50%, 0);
+  width: 60px;
+  height: 250px;
+  background-image: url("/stem.png");
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-position: center;
+`;
 
 const Dev = () => {
   return (
-    <>
-      <RotatingClover></RotatingClover>
-    </>
+    <LuckyCloverContainer>
+      <LuckyCloverText>박하연을 위한 네잎클로버</LuckyCloverText>
+      <LuckyCloverWrapper>
+        <LuckyCloverLeaves />
+        <LuckyCloverStem />
+      </LuckyCloverWrapper>
+    </LuckyCloverContainer>
   );
 };
 
